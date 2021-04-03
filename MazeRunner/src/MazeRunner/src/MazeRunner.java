5559e6eed2703d55bf5718b067f726b8c0f1709f
ssnyder85 <81821808+ssnyder85@users.noreply.github.com>
2021-04-03 00:38:47 -0400

    Add files via upload

---
+++ MazeRunner/src/MazeRunner/src/MazeRunner.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MazeRunner extends Application {
+	ScreenCustomizer sCustom = new ScreenCustomizer();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage mainScreen) throws Exception {
+		// TODO Auto-generated method stub
+		mainScreen.initStyle(StageStyle.UNDECORATED);
+		mainScreen.initStyle(StageStyle.TRANSPARENT);
+		mainScreen.setScene(sCustom.constructScene());
+		mainScreen.show();
+	}
+
+}
