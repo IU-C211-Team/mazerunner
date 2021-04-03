77ac02e236cdef417c18f2a32a7878100fafeb6b
ssnyder85 <81821808+ssnyder85@users.noreply.github.com>
2021-04-02 20:51:18 -0400

    Add files via upload

--- MazeRunner/src/ScreenCustomizer.java
+++ MazeRunner/src/ScreenCustomizer.java
@@ -1,14 +1,19 @@
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -24,17 +29,16 @@ public class ScreenCustomizer {
 	private Button close = new Button("X");
 	
 	BackgroundFill backgroundScheme = new BackgroundFill(Color.web("#D3BFA8"), CornerRadii.EMPTY, Insets.EMPTY);
+	BackgroundFill titleBackground = new BackgroundFill(Color.web("#B18E67"), CornerRadii.EMPTY, Insets.EMPTY);
+	
+	Image logo = new Image("logo.png");
+	ImageView logoIV = new ImageView(logo);
+	
+	Region r1 = new Region();
+	Region r2 = new Region();
 	
 	public Scene constructScene() {
 		scene.getStylesheets().add(getClass().getResource("designstyles.css").toExternalForm());
-		
-		close.setTooltip(new Tooltip("Closes App"));
-		close.getStyleClass().add("button-style-closebutton");
-		close.setOnAction(e -> {
-			Node  source = (Node)  e.getSource(); 
-		    Stage stage  = (Stage) source.getScene().getWindow();
-		    stage.close();
-		});
 		
 		border.setTop(setTitleBar());
 		border.setBackground(new Background(backgroundScheme));
@@ -44,8 +48,23 @@ public class ScreenCustomizer {
 	
 	private HBox setTitleBar() {
 		HBox title = new HBox();
+		logoIV.setFitHeight(screenHeight * .1);
+		logoIV.setPreserveRatio(true);
+		HBox.setHgrow(r1, Priority.ALWAYS);
+		HBox.setHgrow(r2, Priority.ALWAYS);
 		
+		close.setTooltip(new Tooltip("Closes App"));
+		close.getStyleClass().add("button-style-closebutton");
+		close.setOnAction(e -> {
+			Node  source = (Node)  e.getSource(); 
+		    Stage stage  = (Stage) source.getScene().getWindow();
+		    stage.close();
+		});
 		
+		title.setAlignment(Pos.TOP_CENTER);
+		title.setPadding(new Insets(screenHeight * .01, screenWidth * .01, screenHeight * .01, screenWidth * .01));
+		title.setBackground(new Background(titleBackground));
+		title.getChildren().addAll(r1, logoIV, r2, close);
 		
 		return title;
 	}
