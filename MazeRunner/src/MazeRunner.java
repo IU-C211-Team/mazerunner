ad15ade13409f6a15a00d40f841da6080c4ddbdb
ssnyder85 <81821808+ssnyder85@users.noreply.github.com>
2021-04-09 09:23:37 -0400

    Add files via upload
    Housekeeping

--- MazeRunner/src/MazeRunner.java
+++ MazeRunner/src/MazeRunner.java
@@ -16,8 +16,8 @@ public class MazeRunner extends Application {
 		mainScreen.initStyle(StageStyle.UNDECORATED);
 		mainScreen.initStyle(StageStyle.TRANSPARENT);
 		mainScreen.setScene(sCustom.constructScene());
-		mainScreen.setX((sCustom.screenWidth - (sCustom.screenWidth * .65)) / 2);
-		mainScreen.setY((sCustom.screenHeight - (sCustom.screenHeight * .75)) / 2);
+		mainScreen.setX((ScreenCustomizer.screenWidth - (ScreenCustomizer.screenWidth * .65)) / 2);
+		mainScreen.setY((ScreenCustomizer.screenHeight - (ScreenCustomizer.screenHeight * .75)) / 2);
 		mainScreen.show();
 	}
 
